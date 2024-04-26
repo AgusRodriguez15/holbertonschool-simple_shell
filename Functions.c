fa19fa8bb51792aa8558a300b6d7f150120d1450
AgusRodriguez15 <8670@holbertonstudents.com>
2024-04-26 01:52:35 +0000

    CorrectSomeFrees

--- Functions.c
+++ Functions.c
@@ -6,26 +6,27 @@
  */
 char **tokenizar(char *str)
 {
-	char *token, *tok, *t, *a, **ar, *delim = " ";
+	char *token, *tok, *t, *dup, **ar, *delim = " ";
 	int cantToks = 0, i = 0, b = 0;
 
-	a = strdup(str);
-	if (a == NULL)
+	dup = strdup(str);
+	if (dup == NULL)
 		return (NULL);
-	tok = strtok(a, delim);
+	tok = strtok(dup, delim);
 	while (tok != NULL)
 	{
 		cantToks++;
 		tok = strtok(NULL, delim);
 	}
+	free(dup);
 	ar = malloc((cantToks + 1) * sizeof(char *));
 	if (ar == NULL)
 	{
-		free(a);
+		free(dup);
 		return (NULL);
 	}
-	t = strdup(str);
-	if (t == NULL)
+	dup = strdup(str);
+	if (dup == NULL)
 		return (NULL);
 	token = strtok(t, delim);
 	while (token != NULL)
@@ -35,13 +36,13 @@ char **tokenizar(char *str)
 		{
 			for (b = 0; b < i; b++)
 				free(ar[b]);
-			free(a), free(t), free(ar);
+			free(dup), free(dup), free(ar);
 			return (NULL);
 		}
 		i++;
 		token = strtok(NULL, delim);
 	}
 	ar[i] = NULL;
-	free(a), free(t);
+	free(dup);;
 	return (ar);
 }
--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main(void)
 		printf("$shellmans ");
 		if (getline(&str, &x, stdin) == -1)
 		{
+			free(str);
 			return (0);
 		}
 			str[strlen(str) - 1] = '\0';
@@ -29,19 +30,22 @@ int main(void)
 	child_pid = fork();
 		if (child_pid == -1)
 		{
+			free(str);
 			perror("Error");
 			return (1);
 		}
 
 		if (child_pid == 0)
 		{
+			free(str);
 			if (execve(arr_toks[0], arr_toks, environ) == -1)
 			{
-				perror("Error:");
+				perror("Error");
 				return (1);
 			}
 		}
 		else
+			free(str);
 			wait(&status);
 	}
 	return (0);
